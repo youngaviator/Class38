7f99294857d70b53ac38d3117bead3b540b0e3e3
youngaviator <75797837+youngaviator@users.noreply.github.com>
2021-03-27 17:47:14 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,67 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+    Car1=createSprite(100,200)
+    Car2=createSprite(300,200)
+    Car3=createSprite(500,200)
+    Car4=createSprite(700,200)
+    Cars=[Car1,Car2,Car3,Car4]
+  } 
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var x=0
+      var y
+      var index=0
+      var display_position = 130;
+      for(var plr in allPlayers){
+       index=index+1
+       x=x+200
+       y=displayHeight-allPlayers[plr].distance
+        if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+}
